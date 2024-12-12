06f6406cc25e49e1e03fc960e8d64cd250649774
diorz38 <diorz38@users.noreply.github.com>
2024-12-12 21:48:21 +0700

    Update traffic-by-device-chart.blade.php

--- resources/views/components/dashboard/traffic-by-device-chart.blade.php
+++ resources/views/components/dashboard/traffic-by-device-chart.blade.php
@@ -17,91 +17,92 @@
     <div id="traffic-by-device"></div>
 </div>
 <script data-navigate-once>
-    document.addEventListener('livewire:navigated', () => {
-        getTrafficChannelsChartOptions = () => {
+    getTrafficChannelsChartOptions = () => {
 
-            let trafficChannelsChartColors = {}
+        let trafficChannelsChartColors = {}
 
-            if (document.documentElement.classList.contains('dark')) {
-                trafficChannelsChartColors = {
-                    strokeColor: '#1f2937'
-                };
-            } else {
-                trafficChannelsChartColors = {
-                    strokeColor: '#ffffff'
-                }
-            }
-
-            return {
-                series: [70, 5, 25],
-                labels: ['Desktop', 'Tablet', 'Phone'],
-                colors: ['#16BDCA', '#FDBA8C', '#1A56DB'],
-                chart: {
-                    type: 'donut',
-                    height: 400,
-                    fontFamily: 'Inter, sans-serif',
-                    toolbar: {
-                        show: false
-                    },
-                },
-                responsive: [{
-                    breakpoint: 430,
-                    options: {
-                        chart: {
-                            height: 300
-                        }
-                    }
-                }],
-                stroke: {
-                    colors: [trafficChannelsChartColors.strokeColor]
-                },
-                states: {
-                    hover: {
-                        filter: {
-                            type: 'darken',
-                            value: 0.9
-                        }
-                    }
-                },
-                tooltip: {
-                    shared: true,
-                    followCursor: false,
-                    fillSeriesColor: false,
-                    inverseOrder: true,
-                    style: {
-                        fontSize: '14px',
-                        fontFamily: 'Inter, sans-serif'
-                    },
-                    x: {
-                        show: true,
-                        formatter: function(_, {
-                            seriesIndex,
-                            w
-                        }) {
-                            const label = w.config.labels[seriesIndex];
-                            return label
-                        }
-                    },
-                    y: {
-                        formatter: function(value) {
-                            return value + '%';
-                        }
-                    }
-                },
-                grid: {
-                    show: false
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                legend: {
-                    show: false
-                },
+        if (document.documentElement.classList.contains('dark')) {
+            trafficChannelsChartColors = {
+                strokeColor: '#1f2937'
             };
+        } else {
+            trafficChannelsChartColors = {
+                strokeColor: '#ffffff'
+            }
         }
 
-        var chart5 = new ApexCharts(document.getElementById('traffic-by-device'),getTrafficChannelsChartOptions());
+        return {
+            series: [70, 5, 25],
+            labels: ['Desktop', 'Tablet', 'Phone'],
+            colors: ['#16BDCA', '#FDBA8C', '#1A56DB'],
+            chart: {
+                type: 'donut',
+                height: 400,
+                fontFamily: 'Inter, sans-serif',
+                toolbar: {
+                    show: false
+                },
+            },
+            responsive: [{
+                breakpoint: 430,
+                options: {
+                    chart: {
+                        height: 300
+                    }
+                }
+            }],
+            stroke: {
+                colors: [trafficChannelsChartColors.strokeColor]
+            },
+            states: {
+                hover: {
+                    filter: {
+                        type: 'darken',
+                        value: 0.9
+                    }
+                }
+            },
+            tooltip: {
+                shared: true,
+                followCursor: false,
+                fillSeriesColor: false,
+                inverseOrder: true,
+                style: {
+                    fontSize: '14px',
+                    fontFamily: 'Inter, sans-serif'
+                },
+                x: {
+                    show: true,
+                    formatter: function(_, {
+                        seriesIndex,
+                        w
+                    }) {
+                        const label = w.config.labels[seriesIndex];
+                        return label
+                    }
+                },
+                y: {
+                    formatter: function(value) {
+                        return value + '%';
+                    }
+                }
+            },
+            grid: {
+                show: false
+            },
+            dataLabels: {
+                enabled: false
+            },
+            legend: {
+                show: false
+            },
+        };
+    }
+    document.addEventListener('livewire:navigated', () => {
+        var chart5 = new ApexCharts(document.getElementById('traffic-by-device'),
+            getTrafficChannelsChartOptions());
         chart5.render();
+        chart5.updateOptions(getTrafficChannelsChartOptions());
     });
     // init again when toggling dark mode
     document.addEventListener('dark-mode', function() {
