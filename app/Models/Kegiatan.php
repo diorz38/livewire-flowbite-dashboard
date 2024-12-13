ae83107a214c5b8b94ce8555fc3c46347555931c
diorz38 <diorz38@users.noreply.github.com>
2024-12-13 22:34:01 +0700

    Update Kegiatan.php

--- app/Models/Kegiatan.php
+++ app/Models/Kegiatan.php
@@ -14,7 +14,7 @@ class Kegiatan extends Model
 
     protected $table = 'kegiatan';
 
-    protected $guarded = [];
+    protected $guarded = ['id'];
 
     protected $casts = [
         'tgl_mulai' => 'date',
