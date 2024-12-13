843d89aaafba63e664daa4fac0fa187b9c569c7d
diorz38 <diorz38@users.noreply.github.com>
2024-12-13 20:30:11 +0700

    delete unused pages

--- resources/views/livewire/admin/permission/table.blade.php
+++ resources/views/livewire/admin/permission/table.blade.php
@@ -40,10 +40,10 @@
                 <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-600">
                     <thead class="bg-gray-100 dark:bg-gray-700">
                         <tr>
-                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+                            <th scope="col" class="p-2 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
                                 Permission Name
                             </th>
-                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+                            <th scope="col" class="p-2 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
                                 Actions
                             </th>
                         </tr>
@@ -51,12 +51,12 @@
                     <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                         @foreach ($permissions as $item)
                         <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <td class="flex items-center p-4 mr-12 space-x-6 whitespace-nowrap">
+                            <td class="flex items-center p-2 space-x-6 whitespace-nowrap">
                                 <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
                                     <div class="text-base font-semibold text-gray-900 dark:text-white">{{ $item->name }}</div>
                                 </div>
                             </td>
-                            <td class="p-4 space-x-2 whitespace-nowrap">
+                            <td class="p-2 space-x-2 whitespace-nowrap">
                                 <button type="button" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                                     @click="$dispatch('dispatch-permissions-table-edit', {id: '{{ $item->id }}'})">
                                     <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -280,6 +280,7 @@ new class extends Component {
                         </svg>
                         <span class="ml-3" sidebar-toggle-item>GitHub Repository</span>
                     </a>
+                    @role('super-admin')
                     <a href="https://flowbite.com/docs/getting-started/introduction/" target="_blank"
                         class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
                         <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
@@ -312,20 +313,17 @@ new class extends Component {
                         </svg>
                         <span class="ml-3" sidebar-toggle-item>Support</span>
                     </a>
+                    @endrole
                 </div>
             </div>
         </div>
         <div class="absolute bottom-0 left-0 justify-center hidden w-full p-4 space-x-4 bg-white lg:flex dark:bg-gray-800"
             sidebar-bottom-menu>
-            <a wire:navigate href="#"
+            <a wire:navigate href="{{ route('profile.show') }}" data-tooltip-target="tooltip-profile"
                 class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
-                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z">
-                    </path>
-                </svg>
+                <x-fwb-o-profile-card class="w-6 h-6"/>
             </a>
-            <a wire:navigate href="https://alamaya.ltd/settings/" data-tooltip-target="tooltip-settings"
+            <a wire:navigate href="{{ route('settings') }}" data-tooltip-target="tooltip-settings"
                 class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
