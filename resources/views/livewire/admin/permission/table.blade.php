1bb512ad6fa7e345188bf0dd88e33b4ef68f7721
diorz38 <diorz38@users.noreply.github.com>
2025-06-16 15:34:43 +0700

    small update in User and Roles Permission UI

--- resources/views/livewire/admin/permission/table.blade.php
+++ resources/views/livewire/admin/permission/table.blade.php
@@ -1,35 +1,4 @@
-{{-- <div class="col-span-full xl:col-span-12 dark:bg-gray-600">
-
-        <!-- Table -->
-        <div class="overflow-x-auto">
-            <div class="flex flex-wrap justify-start px-20 mt-5 ">
-
-                @foreach ($data as $item)
-                <div class="max-w-sm py-6 pr-4">
-                    <div class="flex flex-col h-full p-2 bg-white rounded-lg shadow-sm dark:bg-gray-800">
-                        <div class="flex items-center">
-                            <h2 class="mr-2 text-lg font-medium text-gray-700 dark:text-white">{{ $item->name }}</h2>
-
-                            @role('super-admin')
-                            <button @click="$dispatch('dispatch-permissions-table-edit', {id: '{{ $item->id }}'})"
-                                class="inline-flex items-center justify-center flex-shrink-0 w-6 h-6 text-white bg-yellow-500 rounded-full dark:bg-yellow-500">
-                                <x-fwb-o-edit class="w-4 h-4"/>
-                            </button>
-                            <button @click="$dispatch('dispatch-permissions-table-delete', {id: '{{ $item->id }}', nama: '{{ $item->name }}'})"
-                                class="inline-flex items-center justify-center flex-shrink-0 w-6 h-6 text-white bg-red-500 rounded-full dark:bg-red-500">
-                                <x-fwb-o-trash-bin class="w-4 h-4"/>
-                            </button>
-                            @endrole
-
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-
-            </div>
-            {{ $data->onEachSide(1)->links() }}
-        </div>
-</div> --}}
+<div class="px-12 mt-2 sm:-mx-6 lg:-mx-8">
 <div class="flex flex-col">
     <livewire:admin.permission.delete />
     <livewire:admin.permission.edit />
@@ -76,4 +45,5 @@
         </div>
     </div>
     {{ $permissions->onEachSide(1)->links() }}
+</div>
 </div>
--- resources/views/livewire/admin/role/table.blade.php
+++ resources/views/livewire/admin/role/table.blade.php
@@ -1,34 +1,5 @@
-{{-- <div class="px-20 col-span-full xl:col-span-12 dark:bg-gray-800">
+<div class="px-12 mt-2 sm:-mx-6 lg:-mx-8">
 
-        <!-- Table -->
-        <div class="overflow-x-auto">
-            <div class="flex flex-wrap justify-between mt-5">
-
-                @foreach ($data as $item)
-                <div class="max-w-sm py-2">
-                    <div class="flex flex-col h-full p-2 bg-white rounded-lg shadow-md dark:bg-gray-800">
-                        <div class="flex items-center justify-start mb-3">
-                            <h2 class="mr-2 text-lg font-medium text-gray-700 dark:text-white">{{ $item->name }}</h2>
-                            @if($item->id != 1 || \Auth::user()->id == 1 )
-                            <div>
-                                <button class="inline-flex items-center justify-center flex-shrink-0 w-6 h-6 text-white bg-yellow-500 rounded-full dark:bg-yellow-500"
-                                    @click="$dispatch('dispatch-roles-table-edit', {id: '{{ $item->id }}'})" />
-                                    <x-fwb-o-edit class="w-4 h-4"/>
-                                </button>
-                                <button class="inline-flex items-center justify-center flex-shrink-0 w-6 h-6 text-white bg-red-500 rounded-full dark:bg-red-500"
-                                    @click="$dispatch('dispatch-roles-table-delete', {id: '{{ $item->id }}', nama: '{{ $item->name }}'})"/>
-                                    <x-fwb-o-trash-bin class="w-4 h-4"/>
-                                </button>
-                            </div>
-                            @endif
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-
-            </div>
-        </div>
-</div> --}}
 <div class="flex flex-col">
     <livewire:admin.role.delete />
     <livewire:admin.role.edit />
@@ -76,4 +47,4 @@
     </div>
     {{ $data->onEachSide(1)->links() }}
 </div>
-
+</div>
