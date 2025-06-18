b5ae9ecf29f2f049641aa748ada3144f9ac6425f
diorz38 <diorz38@users.noreply.github.com>
2025-06-18 12:37:23 +0700

    init Branch Project Management

---
+++ database/migrations/2025_06_18_082729_create_projects_table.php
@@ -0,0 +1,102 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('website')->nullable();
+            $table->string('logo')->nullable(); // Path to an organization logo
+            $table->json('settings')->nullable(); // JSON field for additional settings
+            $table->unsignedBigInteger('leader_id')->nullable();
+            $table->foreign('leader_id')->references('id')->on('users')->onDelete('set null');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+            $table->timestamps();
+        });
+
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('leader_id')->nullable();
+            $table->foreign('leader_id')->references('id')->on('users')->onDelete('set null');
+            $table->unsignedBigInteger('organization_id')->nullable();
+            $table->foreign('organization_id')->references('id')->on('organizations')->onDelete('set null');
+            $table->timestamps();
+        });
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('status')->default('active'); // e.g., active, archived
+            $table->unsignedBigInteger('leader_id')->nullable();
+            $table->foreign('leader_id')->references('id')->on('users')->onDelete('set null');
+            $table->string('members')->nullable();
+            $table->unsignedBigInteger('organization_id')->nullable();
+            $table->foreign('organization_id')->references('id')->on('organizations')->onDelete('set null');
+            $table->unsignedBigInteger('department_id')->nullable();
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('set null');
+            $table->date('start_at');
+            $table->date('end_at')->nullable(); // Nullable for ongoing projects
+            $table->string('icon')->nullable(); // Path to an icon image
+            $table->json('settings')->nullable(); // JSON field for additional settings
+            $table->timestamps();
+        });
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('category'); // e.g., planning, excetution, monitoring, evaluation, reporting
+            $table->string('status')->default('active'); // e.g., active, archived
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('set null');
+            $table->date('start_at');
+            $table->date('end_at')->nullable(); // Nullable for ongoing projects
+            $table->string('icon')->nullable(); // Path to an icon image
+            $table->json('settings')->nullable(); // JSON field for additional settings
+            $table->string('file')->nullable(); // Path to an files;
+            $table->timestamps();
+        });
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('status')->default('active'); // e.g., active, archived
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->unsignedBigInteger('assignment_id')->nullable();
+            $table->foreign('assignment_id')->references('id')->on('assignments')->onDelete('set null');
+            $table->date('start_at');
+            $table->date('end_at')->nullable(); // Nullable for ongoing projects
+            $table->string('icon')->nullable(); // Path to an icon image
+            $table->json('settings')->nullable(); // JSON field for additional settings
+            $table->string('file')->nullable(); // Path to an files;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('organizations');
+        Schema::dropIfExists('departments');
+        Schema::dropIfExists('projects');
+        Schema::dropIfExists('assignments');
+        Schema::dropIfExists('tasks');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ use phpDocumentor\Reflection\DocBlock\Tags\Example;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+Route::get('/welcome', function () {
+    return view('welcome');
 })->name('welcome');
 
 Route::middleware([
@@ -18,28 +21,32 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-    Volt::route('/settings', 'settings')->name('settings');
+    Route::group(['prefix'=>'flowbite','as'=>'flowbite.'], function(){
+        Volt::route('/settings', 'settings')->name('settings');
+        Volt::route('/crud/products', 'crud.products')->name('crud.products');
+        Volt::route('/crud/users', 'crud.users')->name('crud.users');
 
-    Volt::route('/crud/products', 'crud.products')->name('crud.products');
-    Volt::route('/crud/users', 'crud.users')->name('crud.users');
+        Volt::route('/page/maintenance', 'page.maintenance')->name('page.maintenance');
+        Volt::route('/page/error404', 'page.p404')->name('page.error404');
+        Volt::route('/page/error500', 'page.p500')->name('page.error500');
+        Volt::route('/page/pricing', 'page.pricing')->name('page.pricing');
 
-    Volt::route('/page/maintenance', 'page.maintenance')->name('page.maintenance');
-    Volt::route('/page/error404', 'page.p404')->name('page.error404');
-    Volt::route('/page/error500', 'page.p500')->name('page.error500');
-    Volt::route('/page/pricing', 'page.pricing')->name('page.pricing');
+        Volt::route('/authentication/sign-in', 'authentication.signin')->name('authentication.signin');
+        Volt::route('/authentication/sign-up', 'authentication.signup')->name('authentication.signup');
+        Volt::route('/authentication/forgot-password', 'authentication.forgot-password')->name('authentication.forgot-password');
+        Volt::route('/authentication/reset-password', 'authentication.reset-password')->name('authentication.reset-password');
+        // Volt::route('/authentication/sign-in', 'authentication.signin')->name('authentication.signin');
 
-    Volt::route('/authentication/sign-in', 'authentication.signin')->name('authentication.signin');
-    Volt::route('/authentication/sign-up', 'authentication.signup')->name('authentication.signup');
-    Volt::route('/authentication/forgot-password', 'authentication.forgot-password')->name('authentication.forgot-password');
-    Volt::route('/authentication/reset-password', 'authentication.reset-password')->name('authentication.reset-password');
-    // Volt::route('/authentication/sign-in', 'authentication.signin')->name('authentication.signin');
+        Volt::route('/playground/stacked', 'playground.stacked')->name('playground.stacked');
+        Volt::route('/playground/sidebar', 'playground.sidebar')->name('playground.sidebar');
 
-    Volt::route('/playground/stacked', 'playground.stacked')->name('playground.stacked');
-    Volt::route('/playground/sidebar', 'playground.sidebar')->name('playground.sidebar');
+        Route::get('/apex', ExampleChart::class)->name('apex.chart');
 
-    Route::get('/apex', ExampleChart::class)->name('apex.chart');
+
+    });
 
     Route::get('/kegiatan', KegiatanIndex::class)->name('kegiatan.index');
+    Volt::route('/settings', 'settings')->name('settings');
 
     Route::group(['middleware' => ['role:super-admin']], function () {
         Route::get('/admin/user', AdminUserTable::class)->name('admin.user');
